5148d866195cdfa11ce1e50deeda32335f547b02
Marko Sandalj <msandalj@netgen.io>
2021-10-02 18:28:42 +0200

    change db host

--- src/gradmin/settings.py
+++ src/gradmin/settings.py
@@ -97,8 +97,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'gradmin',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'HOST': 'https://theehhdude23.eu.pythonanywhere.com/',
+        # 'PORT': '3306',
         'USER': 'root',
         'PASSWORD': 'root',
     }
