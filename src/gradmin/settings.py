607f228682ae0023d8039b9dc42d6a9b55f8a83d
Marko Sandalj <msandalj@netgen.io>
2021-10-02 17:42:28 +0200

    add static root

--- src/gradmin/settings.py
+++ src/gradmin/settings.py
@@ -147,7 +147,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "static",
+STATIC_ROOT = str(BASE_DIR / "static"),
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
