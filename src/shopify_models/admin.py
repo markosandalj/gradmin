63c5646f33c287f86c63e7a9d80a2b6afba54d0f
Marko Sandalj <msandalj@netgen.io>
2022-06-05 23:19:23 +0200

    better os error msg

--- src/shopify_models/admin.py
+++ src/shopify_models/admin.py
@@ -194,7 +194,7 @@ class PageAdmin(admin.ModelAdmin):
                         print(response.json())
                         messages.success(request, "Page {page} uspješno ažuriran sa zadatcima".format(page=page.title))
                     except:
-                        messages.error(request, "Page {page} neuspješno ažuriran. Error: {err}".format(page=page.title, err=sys.exc_info()[0]))
+                        messages.error(request, "Page {page} neuspješno ažuriran. Error: {err}, {err2}, {err3}".format(page=page.title, err=sys.exc_info()[0], err2=sys.exc_info()[1], err2=sys.exc_info()[2]))
             except:
                 messages.error(request, "Page {page} nema section?. Error: {err}".format(page=page.title, err=sys.exc_info()[0]))
 
